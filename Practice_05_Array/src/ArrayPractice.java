22138cbd0d8004c4278b06ff26a9b388938f2892
truuuely <kmyc7030@gmail.com>
2025-07-09 21:53:05 +0900

    add: 배열 실습문제

--- Practice_05_Array/src/ArrayPractice.java
+++ Practice_05_Array/src/ArrayPractice.java
@@ -7,15 +7,15 @@ public class ArrayPractice {
 
     public static void main(String[] args) {
         ArrayPractice ap = new ArrayPractice();
-//        ap.practice1();
-//        ap.practice2();
-//        ap.practice3();
-//        ap.practice4();
-//        ap.practice5();
-//        ap.practice6();
-//        ap.practice7();
-//        ap.practice8();
-//        ap.practice9();
+        ap.practice1();
+        ap.practice2();
+        ap.practice3();
+        ap.practice4();
+        ap.practice5();
+        ap.practice6();
+        ap.practice7();
+        ap.practice8();
+        ap.practice9();
         ap.practice10();
     }
 
